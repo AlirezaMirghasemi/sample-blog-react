211c4d2de7e1da3f92f1ec22e447bc0cfa9425ea
AlirezaMirghasemi <alireza.mirghasemi@gmail.com>
2025-02-13 16:27:22 +0330

    use await async in delete post

--- src/components/pages/PostDelete.jsx
+++ src/components/pages/PostDelete.jsx
@@ -7,30 +7,39 @@ const PostDelete = () => {
   const [errors, setErrors] = useState();
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const deletePost = (e) => {
+
+  const deletePost = async () => {
     setLoading(true);
-    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
-      method: "DELETE",
-    })
-      .then(() => {
-        setLoading(false);
-        setErrors("");
-        Swal.fire({
-          title: "Success",
-          text: "Your post deleted successfully!",
-          icon: "danger",
-        });
-        navigate("/");
-      })
-      .catch((e) => {
-        setErrors(e.message);
-        setLoading(false);
-        Swal.fire({
-          title: "Error",
-          text: e.message,
-          icon: "warning",
-        });
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${postId}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      let json = await response.json();
+
+      setLoading(false);
+      setErrors("");
+      Swal.fire({
+        title: "Success",
+        text: "Your post deleted successfully!",
+        icon: "success",
       });
+    } catch (e) {
+      setErrors(e.message);
+      setLoading(false);
+      Swal.fire({
+        title: "Error",
+        text: e.message,
+        icon: "warning",
+      });
+    } finally {
+      navigate("/");
+    }
   };
 
   return (
